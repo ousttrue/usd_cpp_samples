994afadcb0ba3dc744d7d51b1835c5895d167316
ousttrue <ousttrue@gmail.com>
2022-01-11 10:50:50 +0900

    pydear + usd

--- run_usd.py
+++ run_usd.py
@@ -23,8 +23,13 @@ CONFIG_FILE = pathlib.Path("window.ini")
 
 HERE = pathlib.Path(__file__).absolute().parent
 
+# setup USD
 sys.path.insert(0, str(HERE / 'build_release/lib/python'))
 os.environ['PATH'] = f'{HERE / "build_release/bin"};{HERE / "build_release/lib"};{os.environ["PATH"]}'
+from pxr import Usd  # nopep8
+from pxr import UsdImagingGL  # nopep8
+from pxr.Gf import Vec4f, Vec4d, Camera, Matrix4d, Vec3d  # nopep8
+from pxr.Glf import SimpleMaterial  # nopep8
 
 
 class ImguiDocks:
@@ -49,8 +54,76 @@ class ImguiDocks:
 
 
 class SampleController(PydearController):
+    def __init__(self, scale: float = 1):
+        super().__init__(scale=scale)
+
+        # load usd
+        # path = pathlib.Path(os.environ['USERPROFILE']) / \
+        #     "Desktop/Kitchen_set/Kitchen_set.usd"
+        path = HERE / 'test_sphere.usda'
+        assert path.exists()
+
+        self.engine = None
+
+        self.stage: Usd.Stage = Usd.Stage.Open(str(path))
+
+        self.params = UsdImagingGL.RenderParams()
+        self.params.clearColor = Vec4f(1.0, 0.5, 0.1, 1.0)
+        # params.frame = Usd.TimeCode.Default()
+        self.params.frame = 1.0
+        self.params.complexity = 1.0
+        self.params.forceRefresh = False
+        self.params.enableLighting = False
+        # params.enableLighting = True
+        self.params.enableSceneMaterials = True
+        self.params.drawMode = UsdImagingGL.DrawMode.DRAW_SHADED_SMOOTH
+        self.params.highlight = True
+        self.params.gammaCorrectColors = False
+        # params.colorCorrectionMode = TfToken("sRGB");
+        self.params.showGuides = True
+        self.params.showProxy = True
+        self.params.showRender = False
+        self.params.forceRefresh = True
+
+        material = SimpleMaterial()
+        ambient = Vec4f()
+
+        self.camera = Camera()
+        # self.camera.SetPerspectiveFromAspectRatioAndFieldOfView(16.0 / 9.0, 60, Camera.FOVHorizontal)
+        # self.camera.focus_distance = 100
+        dist = 100
+        trans = Matrix4d()
+        trans.SetTranslate(Vec3d.ZAxis() * dist)
+        # GfMatrix4d roty(1.0);
+        # GfMatrix4d rotz(1.0);
+        # GfMatrix4d rotx(1.0);
+        # roty.SetRotate(GfRotation(GfVec3d::YAxis(), -rotation[0]));
+        # rotx.SetRotate(GfRotation(GfVec3d::XAxis(), -rotation[1]));
+        # rotz.SetRotate(GfRotation(GfVec3d::ZAxis(), -rotation[2]));
+        # GfMatrix4d toCenter;
+        # toCenter.SetTranslate(center);
+        self.camera.transform = trans
+        self.camera.focus_distance = dist
+
     def imgui_create_docks(self):
         return ImguiDocks()
+
+    def lazy_init(self):
+        if self.engine:
+            return
+        self.engine = UsdImagingGL.Engine()
+
+    def draw_scene(self):
+        self.lazy_init()
+        frustum = self.camera.frustum
+        # engine.SetLightingState(_lights, _material, _ambient);
+        self.engine.SetRenderViewport(Vec4d(0, 0, *controller.viewport))
+        # engine.SetWindowPolicy(CameraUtilConformWindowPolicy::CameraUtilMatchHorizontally);
+        # If using a usd camera, use SetCameraPath renderer.SetCameraPath(sceneCam.GetPath())
+        # else set camera state
+        self.engine.SetCameraState(frustum.ComputeViewMatrix(
+        ), frustum.ComputeProjectionMatrix())
+        self.engine.Render(self.stage.GetPseudoRoot(), self.params)
 
 
 if __name__ == "__main__":
@@ -66,17 +139,6 @@ if __name__ == "__main__":
         config=WindowConfig.load_json_from_path(CONFIG_FILE),
         title="pydear")
 
-    path = pathlib.Path(os.environ['USERPROFILE']) / \
-        "Desktop/Kitchen_set/Kitchen_set.usd"
-    assert path.exists()
-
-    from pxr import Usd
-    stage: Usd.Stage = Usd.Stage.Open(str(path))
-
-    from pxr import UsdImagingGL
-    engine = UsdImagingGL.Engine()
-    renderParams = UsdImagingGL.RenderParams()
-
     # main loop
     lastCount = 0
     while True:
@@ -84,14 +146,12 @@ if __name__ == "__main__":
         if not count:
             break
 
-        # _renderer->SetCameraState(GetCurrentCamera().GetFrustum().ComputeViewMatrix(),GetCurrentCamera().GetFrustum().ComputeProjectionMatrix());
-        engine.Render(stage.GetPseudoRoot(), renderParams)
-
         d = count - lastCount
         lastCount = count
         if d > 0:
             controller.onUpdate(d)
             controller.draw()
+
             loop.end_frame()
 
     # save window config
