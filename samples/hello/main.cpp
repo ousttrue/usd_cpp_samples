ca330319ad9c5405459614b5c0e868c2fe8224ba
ousttrue <ousttrue@gmail.com>
2023-07-16 09:39:36 +0900

    build usd_plug.dll, usd_ar.dll, usd_sdf.dll

--- samples/hello/main.cpp
+++ samples/hello/main.cpp
@@ -4,7 +4,7 @@
 
 int main()
 {
-    // auto stage = pxr::UsdStage::CreateInMemory();
+    auto stage = pxr::UsdStage::CreateInMemory();
     // auto sphere = pxr::UsdGeomSphere::Define(stage, pxr::SdfPath("/TestSphere"));
     // auto root = stage->GetRootLayer();
     // root->Export("test_sphere.usda");
